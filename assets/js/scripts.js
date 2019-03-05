792d89047788785b5b45457b20934cd4aab57387
denise martin <denise.martin@vanderbilt.edu>
2019-03-04 21:41:26 -0600

    changes to js buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,15 +2,15 @@ $(document).ready (function() {
 
     
     
-    var topics = ["sala dancing", "tap dancing", "flamenco dancing", "paso doble dancing","hip hop dancing","ballet dancing"];
+    var topics = ["salsa dancing", "tap dancing", "flamenco dancing", "paso doble dancing","hip hop dancing","ballet dancing"];
     
     displayButtons();
     
     function displayButtons() {
         $(".buttons").empty();
         
-        for (let i = 0; i < topics.length; i++) {
-            var btn = `<button class="renderBtn> ${topics[i]} </button>`
+        for (var i = 0; i < topics.length; i++) {
+            var btn = `<button class="renderBtn"> ${topics[i]} </button>`
             
             $(".buttons").append(btn);
             
