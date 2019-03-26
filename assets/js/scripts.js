3880e24e76656db63196dc84949610277f13f1f4
denise martin <denise.martin@vanderbilt.edu>
2019-03-25 19:54:58 -0500

    changes to js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,74 +1,73 @@
-$(document).ready(function () {
 
 
 
-    var topics = ["salsa dancing", "tap dancing", "flamenco dancing", "paso doble dancing", "hip hop dancing", "ballet dancing"];
+var topics = ["salsa dancing", "tap dancing", "flamenco dancing", "paso doble dancing", "hip hop dancing", "ballet dancing"];
 
-    displayButtons();
+displayButtons();
 
-    function displayButtons() {
-        $(".buttons").empty();
+function displayButtons() {
+    $(".buttons").empty();
 
-        for (var i = 0; i < topics.length; i++) {
-            var btn = `<button class="renderBtn"> ${topics[i]} </button>`
+    for (var i = 0; i < topics.length; i++) {
+        var btn = `<button class="renderBtn"> ${topics[i]} </button>`
 
-            $(".buttons").append(btn);
+        $(".buttons").append(btn);
 
-        }
     }
-});
+}
 
-/*
 //event listner for buttons
-$(".button").on("click", function() {
-var dance = $(this).attr("data-dance");
-//Query Url that will search giphy
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + dance + "&api_key=Zt5bq1Jj1g5t5azZGfuSFcRZnX3dsuIz&limit=10";
+$(document).on("click", ".button", function () {
+    var dance = $(this).attr("data-dance");
+    //Query Url that will search giphy
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + dance + "&api_key=Zt5bq1Jj1g5t5azZGfuSFcRZnX3dsuIz&limit=10";
 
-//Ajax Get Request
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  // After the data comes back from the API
-  
-    .then(function(response) {
-      // Storing an array of results in the results variable
+    //Ajax Get Request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // After the data comes back from the API
 
-      var results = response.data;
+        .then(function (response) {
+            // Storing an array of results in the results variable
 
-      // Looping for result item
-      for (var i = 0; i < results.length; i++) {
+            var results = response.data;
 
-        // rating
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            console.log(results);
 
-          // Creating a div for the gif
-          var gifDiv = $("<div>");
+            // Looping for result item
+            for (var i = 0; i < results.length; i++) {
 
-          // Storing the result item's rating
-          var rating = results[i].rating;
+                // rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + rating);
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
 
-          // Creating an image tag
-          var danceImage = $("<img>");
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
 
-          // Giving the image tag an src attribute of a proprty pulled off the
-          // result item
-          danceImage.attr("src", results[i].images.fixed_height.url);
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
 
-          // Appending the paragraph and danceImage we created to the "gifDiv" div we created
-          gifDiv.append(p);
-          gifDiv.append(danceImage);
+                    // Creating an image tag
+                    var danceImage = $("<img>");
 
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      }
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    danceImage.attr("src", results[i].images.fixed_height.url);
+
+                    // Appending the paragraph and danceImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(danceImage);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            }
 
 
 
-})
-*/
+        })
+});
